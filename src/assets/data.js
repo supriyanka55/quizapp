5b3461f4c388fa34b3fedd2c52a17601cee34dad
supriya <supriyankapolinaty5@gmail.com>
2024-05-18 15:52:48 +0530

    quiz app creation completed

---
+++ src/assets/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+      question: "Who is the current captain of Indian cricket team? ",
+      option1: "Rohit Sharma",
+      option2: "Virat Kohli",
+      option3: "Jasprit Bumrah",
+      option4: "Yuzvendra chahal",
+      ans: 1,
+    },
+    {
+      question: "what is the  highest individual score in ODI cricket?",
+      option1: "209",
+      option2: "183",
+      option3: "208",
+      option4: "264",
+      ans: 4,
+    },
+    {
+      question: "How many 100's has virat kohli scored in international cricket",
+      option1: "80",
+      option2: "100",
+      option3: "20",
+      option4: "85",
+      ans: 1,
+    },
+    {
+      question: "capital of INDIA?",
+      option1: "Hyderabad",
+      option2: "Mumbai",
+      option3: "Delhi",
+      option4: "Banglore",
+      ans: 3,
+    },
+    {
+      question: "Which is the biggest continent in the world?",
+      option1: "Africa",
+      option2: "Asia",
+      option3: "Australia",
+      option4: "Europe",
+      ans: 2,
+    },
+    
+
+  ];
